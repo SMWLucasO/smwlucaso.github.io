9d7ef0bad9708e17fed2567654289dad16ef0b17
Lucas Ouwens <lucasouwens@gmail.com>
2020-01-31 20:21:00 +0100

    Implemented projects scrollbar & loading projects from the json data file.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,73 @@
-"use strict";
-
 (function () {
-    let currentIndex = 0;
+    'use strict';
+
+    let currentIndex = 1;
+
     document.getElementById("next").onclick = (click) => {
-        alert("A");
+        clearActiveItems();
+
+        let projects = document.getElementsByClassName('gallery-item');
+        if((currentIndex) >= projects.length-1)
+            currentIndex = -1;
+
+        projects[++currentIndex].classList.add('active-item');
+        projects[++currentIndex].classList.add('active-item');
     };
 
     document.getElementById("prev").onclick = (click) => {
-        alert("B");
+        clearActiveItems();
+        let projects = document.getElementsByClassName('gallery-item');
+
+        if((currentIndex-2) < 0)
+            currentIndex = projects.length;
+
+        projects[--currentIndex].classList.add('active-item');
+        projects[--currentIndex].classList.add('active-item');
     };
+
+    function clearActiveItems() {
+        [...document.getElementsByClassName('active-item')].forEach((el) => el.classList.remove('active-item'));
+    }
+
+    function setupProjects() {
+        fetch('assets/data/projects.json')
+            .then(returned => returned.json())
+            .then(outcome => {
+                generateTemplatingForProjects(outcome, generateProjectHTML);
+            });
+    }
+
+    function generateTemplatingForProjects(projectJSONData) {
+        fetch('templates/project.mustache')
+            .then(data => data.text())
+            .then(template => {
+                for (let project in projectJSONData.projects) {
+                    generateProjectHTML(template, project, projectJSONData.projects[project]);
+                }
+            }).finally(() => {
+            showInitialProjectsInHTML();
+        });
+    }
+
+    function generateProjectHTML(template, projectName, project) {
+        let rendered = Mustache.render(template,
+            {
+                name: projectName,
+                description: project.description,
+                src: project.src,
+                tags: project.tags
+            }
+        );
+
+        document.getElementById("projects-placeholder").innerHTML += rendered;
+    }
+
+    function showInitialProjectsInHTML() {
+        let galleryItems = document.getElementsByClassName('gallery-item');
+        galleryItems[0].classList.add('active-item');
+        galleryItems[1].classList.add('active-item');
+    }
+
+    setupProjects();
+    console.log(currentIndex-2);
 })();
