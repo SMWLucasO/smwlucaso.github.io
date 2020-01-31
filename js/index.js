1122b3ccd27c317319a5310445ec6afc83d5ca39
Lucas Ouwens <lucasouwens@gmail.com>
2020-01-31 16:56:22 +0100

    Started working on interaction, created 'projects' part

---
+++ js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function () {
+    let currentIndex = 0;
+    document.getElementById("next").onclick = (click) => {
+        alert("A");
+    };
+
+    document.getElementById("prev").onclick = (click) => {
+        alert("B");
+    };
+})();
